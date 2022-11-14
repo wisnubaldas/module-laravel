baa00498f32f14063bdd8154bcb832abe17db34b
wisnubaldas <wisnubaldas@gmail.com>
2022-11-14 12:34:44 +0700

    ah

--- src/BootScript.php
+++ src/BootScript.php
@@ -7,23 +7,25 @@ class BootScript
     {
         $ph = '<?php'.PHP_EOL;
         $using = 'use Wisnubaldas\CleanClass\Driver\RouteStub;'.PHP_EOL;
-        $content = \file_get_contents('../../'.'routes/web.php');
+        $content = \file_get_contents(dirname(__DIR__, 4).'/routes/web.php');
         $content = str_replace('<?php','',$content);
         $fungsinya = <<<END
+        
         RouteStub::get_file(base_path('routes/Web/'));
         END;
 
         $scrf = $ph.$using.$content.$fungsinya;
-        \file_put_contents('../../'.'routes/web.php',$scrf);
+        \file_put_contents(dirname(__DIR__, 4).'/routes/web.php',$scrf);
 
         // untuk api nya
-        $content = \file_get_contents('../../'.'routes/api.php');
+        $content = \file_get_contents(dirname(__DIR__, 4).'/routes/api.php');
         $content = str_replace('<?php','',$content);
         $fungsinya = <<<END
+
         RouteStub::get_file(base_path('routes/Api/'));
         END;
         $scrf = $ph.$using.$content.$fungsinya;
-        \file_put_contents('../../'.'routes/api.php',$scrf);
+        \file_put_contents(dirname(__DIR__, 4).'/routes/api.php',$scrf);
     }
 }
 
